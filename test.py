2f1cfd46c14de56fee0431c52cc0350a093c40ab
klei22 <kauna@avalonwest.tech>
2023-09-06 23:36:24 -0700

    Add circular arrangement

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 from pyvis.network import Network
+import numpy as np
+
 def dot_product(v1, v2):
     # Flatten the vectors
     flat_v1 = [item for sublist in v1 for item in sublist]
     flat_v2 = [item for sublist in v2 for item in sublist]
-    
+
     # Pad vectors to make them of equal length
     while len(flat_v1) < len(flat_v2):
         flat_v1.append(0)
     while len(flat_v2) < len(flat_v1):
         flat_v2.append(0)
-    
+
     return sum(x*y for x, y in zip(flat_v1, flat_v2))
 
 # def dot_product(v1, v2):
@@ -18,7 +20,7 @@ def dot_product(v1, v2):
 #         v1.append([0, 0, 0, 0, 0])
 #     while len(v2) < len(v1):
 #         v2.append([0, 0, 0, 0, 0])
-    
+
 #     return sum(x*y for x, y in zip(v1, v2))
 
 languages = ['French', 'German', 'Hindi', 'Russian', 'Arabic', 'Indonesian', 'Tagalog', 'Japanese', 'Korean', 'Swahili', 'Portuguese', 'Urdu']
@@ -55,9 +57,30 @@ for lang1 in languages:
 
 print(markdown)
 
-def dynamic_plot(matrix):
+
+def _circular_positions(labels, radius=10000):
+    """
+    Generate circular positions for given labels.
+
+    Parameters:
+    - labels: List of labels
+    - radius: Radius of the circle
+
+    Returns:
+    - Dictionary with labels as keys and positions as values
+    """
+    theta = 2 * 3.141592653589793 / len(labels)
+    positions = {}
+    for i, label in enumerate(labels):
+        x = radius * np.cos(i * theta)
+        y = radius * np.sin(i * theta)
+        positions[label] = (x, y)
+    return positions
+
+
+def circular_plot(matrix):
     nt = Network(notebook=True, width="100%", height="750px", bgcolor="#222222", font_color="white", heading="")
-    
+
     # Use remote resources
     nt.use_CDN = True
 
@@ -70,11 +93,45 @@ def dynamic_plot(matrix):
         for lang2 in matrix:
             weight = matrix[lang1][lang2]
             if lang1 != lang2:
-                nt.add_edge(lang1, lang2, value=weight/10.0)
-    
+                nt.add_edge(lang1, lang2, value=weight)
+
+    # Use the circular layout for nodes
+    positions = _circular_positions(list(matrix.keys()))
+
+    # Update node positions
+    for node in nt.nodes:
+        label = node['label']
+        position = positions.get(label, (0, 0))
+        node['x'] = position[0]
+        node['y'] = position[1]
+
+    # Display the plot
+    nt.show("temp.html")
+
+
+# Create the circular plot
+circular_plot(similarity_matrix)
+
+def dynamic_plot(matrix):
+    nt = Network(notebook=True, width="100%", height="750px", bgcolor="#222222", font_color="white", heading="")
+
+    # Use remote resources
+    nt.use_CDN = True
+
+    # Add nodes
+    for lang in matrix:
+        nt.add_node(lang)
+
+    # Add edges
+    for lang1 in matrix:
+        for lang2 in matrix:
+            weight = matrix[lang1][lang2]
+            if lang1 != lang2:
+                nt.add_edge(lang1, lang2, value=weight)
+
     # Display the dynamic plot
     nt.show("temp.html")
 
 # Create the dynamic plot
-dynamic_plot(similarity_matrix)
+# dynamic_plot(similarity_matrix)
 
